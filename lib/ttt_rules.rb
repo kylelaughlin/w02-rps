3bf02ce40e4a7592da546b5b64b4f2452733c62f
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-27 11:06:19 -0500

    Create tic-tac-toe rules file initialize and getters and setters

---
+++ lib/ttt_rules.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class TTTRules
+
+  attr_reader :acceptable_choices
+  attr_accessor :player_one, :player_two
+
+  def initialize(player_one:, player_two:)
+    @player_one = player_one
+    @player_two = player_two
+    @acceptable_choices = [ 'Top Left',
+                            'Top Center',
+                            'Top Right',
+                            'Middle Left',
+                            'Middle Center',
+                            'Middle Right',
+                            'Bottom Left',
+                            'Bottom Center',
+                            'Bottom Right'
+                          ]
+  end
+
+end
