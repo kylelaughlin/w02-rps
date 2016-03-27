d680a59a07e321fc0b981666ef33d77ff82c42ae
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-27 13:31:39 -0500

    Added a print tic-tac-toe board method

--- lib/ttt_rules.rb
+++ lib/ttt_rules.rb
@@ -8,18 +8,18 @@ class TTTRules
   def initialize(player_one:, player_two:)
     @player_one = player_one
     @player_two = player_two
-    @acceptable_choices = [ 'Top Left',
-                            'Top Center',
-                            'Top Right',
-                            'Middle Left',
-                            'Middle Center',
-                            'Middle Right',
-                            'Bottom Left',
-                            'Bottom Center',
-                            'Bottom Right'
-                          ]
+    @acceptable_choices = [[1,2,3],[4,5,6],[7,8,9]]
   end
 
-  
+  def run_a_round
+  end
+
+  def print_board(acceptable_choices)
+    print " #{acceptable_choices[0][0]} | #{acceptable_choices[0][1]} | #{acceptable_choices[0][2]}"
+    print "\n-----------\n"
+    print " #{acceptable_choices[1][0]} | #{acceptable_choices[1][1]} | #{acceptable_choices[1][2]}"
+    print "\n-----------\n"
+    print " #{acceptable_choices[2][0]} | #{acceptable_choices[2][1]} | #{acceptable_choices[2][2]} \n"
+  end
 
 end
